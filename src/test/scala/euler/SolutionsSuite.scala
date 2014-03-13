6a3fb0bc2ff8a40e5114501a9f61753db83492f2
Erik J. Sturcke <erik@carbonbox.com>
2014-03-12 23:29:03 -0400

    Remove old tests

--- src/test/scala/euler/SolutionsSuite.scala
+++ src/test/scala/euler/SolutionsSuite.scala
@@ -4,16 +4,9 @@ import org.scalatest._
 
 class SolutionsSuite extends FunSuite with Matchers {
 
-  /*
-  "The problems" should "have the correct solutions" in {
-    Problem1.solve should be (  233168 )
-    Problem2.solve should be ( 4613732 )
-    Problem3.solve should be (    6857 )
-    Problem4.solve should be (  906609 )
-  }
-*/
   test("Problem 1") { Problem1.solve should be (  233168 ) }
   test("Problem 2") { Problem2.solve should be ( 4613732 ) }
   test("Problem 3") { Problem3.solve should be (    6857 ) }
   test("Problem 4") { Problem4.solve should be (  906609 ) }
+
 }
