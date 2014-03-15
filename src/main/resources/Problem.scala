74ec87beec81ae97cd3b4060ae30547814a96092
Erik J. Sturcke <erik@carbonbox.com>
2014-03-15 00:49:45 -0400

    Add new problem template

---
+++ src/main/resources/Problem.scala
@@ -0,0 +1,22 @@
+package euler
+
+/**
+ * '''@title'''
+ *
+ * [[http://projecteuler.net/problem=@n Project Euler Problem @n]]
+ *
+ * @description
+ *
+ * '''Solution'''
+ *
+ */
+
+object Problem@n extends Problem {
+
+  def id    = @n
+  def title = "@title"
+
+  /** Returns ... */
+  def solve = ...
+
+}
